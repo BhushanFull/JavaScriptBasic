197a744a8a38631928a3d2cc21c29493f3a5e4d6
unknown <kumarbhushansingh491@gmail.com>
2020-08-10 17:59:30 +0530

    ArrayFunctions

---
+++ JavaScript/ArrayFunctions.js
@@ -0,0 +1,66 @@
+// var age = [23,25,1,78,10,44,51,45,20,30,4,34,56,78,2,455,77,88,1000,123231,213];
+
+class ArrayManipulations{
+    addElement(age,value){
+        var startTime = new Date().getTime();
+        age.push(value);
+        var endTime = new Date().getTime();
+        console.log("total time for add : ",endTime-startTime);
+        return age;
+    };
+
+     removeElement(age){
+        var startTime = new Date().getTime();
+        age.pop();
+        var endTime = new Date().getTime();
+        console.log("total time for remove : ",endTime-startTime);
+        return age;
+    }
+
+     reverseArray(age){
+        var startTime = new Date().getTime();
+        age.reverse();
+        var endTime = new Date().getTime();
+        console.log("total time for reverse : ",endTime-startTime);
+        return age;
+    }
+     sortArray(age){
+        var startTime = new Date().getTime();
+        age.sort(function(a,b){return a-b;});
+        var endTime = new Date().getTime();
+        console.log("total time for sorting : ",endTime-startTime);
+        return age;
+    }
+
+    // function lengthOfArray(age){
+    //     return age.length;
+    // }
+
+     findMaxInElement(age){
+        return Math.max(...age);
+        
+    }
+
+     findIndexOfElement(age,value){
+        return age.indexOf(value);
+    }
+
+     serchElementIsPresent(age,value){
+        return age.includes(value);
+    }
+}
+
+module.exports = ArrayManipulations;
+
+// console.log(age.reduce(function(a,b){
+//     return Math.max(a,b);
+// }));
+
+// console.log(addElement(age,111));
+// console.log(removeElement(age));
+// console.log(reverseArray(age));
+// console.log(sortArray(age));
+// console.log(lengthOfArray());
+// console.log(findMaxInElement(age));
+// console.log(findIndexOfElement(age,45));
+// console.log(serchElementIsPresent(age,111));
---
+++ Jest/ArrayFunctions.test.js
@@ -0,0 +1,23 @@
+var ArrayManipulations = require('../JavaScript/ArrayFunctions');
+
+var myObject = new ArrayManipulations();
+test('Array functions',()=>{
+    var age = [23,45,1,3,67,8,33,555,300,9,200];
+    age.push(111);
+    expect(myObject.addElement(age,111)).toContain(111);
+
+    age.pop();
+    expect(myObject.removeElement(age)).not.toContain(111);
+
+    // age.reverse();
+    // expect(myObject.reverseArray(age)).toContain(age);
+
+    // age.sort();
+    // expect(myObject.sortArray(age)).toContain(age);
+
+    expect(myObject.findMaxInElement(age)).toBe(555);
+
+    expect(myObject.findIndexOfElement(age,3)).toBe(3);
+
+    expect(myObject.serchElementIsPresent(age,9)).toBeTruthy();
+})
